bcefd66cf76355ed922ed3766b5a2006861d3faf
Dawid Drozd <drozddawid.uam@gmail.com>
2016-05-23 13:44:03 +0200

    Add debug utils for printing std::istream

---
+++ src/acme/DebugUtils.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Dawid Drozd aka Gelldur on 5/23/16.
+//
+
+#include "DebugUtils.h"
+#include <log.h>
+#include <sstream>
+
+namespace Debug
+{
+
+std::string print(std::istream& stream)
+{
+	std::string tmpContent(std::istreambuf_iterator<char>(stream), {});
+	DLOG(tmpContent.c_str());
+
+	return tmpContent;
+}
+
+}
---
+++ src/acme/DebugUtils.h
@@ -0,0 +1,14 @@
+//
+// Created by Dawid Drozd aka Gelldur on 5/23/16.
+//
+
+#pragma once
+
+#include <istream>
+
+namespace Debug
+{
+
+std::string print(std::istream& stream);
+
+}
