b888abe05c1e8e73d65bea6cb34fe0dbebda14e9
Dawid Drozd <drozddawid.uam@gmail.com>
2016-05-25 09:07:13 +0200

    Add log flush for easier debug

--- proj/linux/log.h
+++ proj/linux/log.h
@@ -6,8 +6,8 @@
 
 #include <cstdio>
 
-#define  DLOG(...)  printf("DLOG:\t");printf(__VA_ARGS__);printf("\n")
-#define  ILOG(...)  printf("ILOG:\t");printf(__VA_ARGS__);printf("\n")
-#define  WLOG(...)  printf("WLOG:\t");printf(__VA_ARGS__);printf("\n")
-#define  ELOG(...)  printf("ELOG:\t");printf(__VA_ARGS__);printf("\n")
-#define  FLOG(...)  printf("FLOG:\t");printf(__VA_ARGS__);printf("\n")
+#define  DLOG(...)  printf("DLOG:\t");printf(__VA_ARGS__);printf("\n");fflush(stdout);
+#define  ILOG(...)  printf("ILOG:\t");printf(__VA_ARGS__);printf("\n");fflush(stdout);
+#define  WLOG(...)  printf("WLOG:\t");printf(__VA_ARGS__);printf("\n");fflush(stdout);
+#define  ELOG(...)  printf("ELOG:\t");printf(__VA_ARGS__);printf("\n");fflush(stdout);
+#define  FLOG(...)  printf("FLOG:\t");printf(__VA_ARGS__);printf("\n");fflush(stdout);
