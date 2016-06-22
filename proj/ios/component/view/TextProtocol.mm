a89920221f4d85d0e073a28da8a1129759c44e6e
Dawid Drozd <drozddawid.uam@gmail.com>
2016-06-22 12:46:12 +0200

    Add TextProtocol impl for iOS

--- proj/ios/component/view/TextProtocol.mm
+++ proj/ios/component/view/TextProtocol.mm
@@ -2,22 +2,34 @@
 // Created by Dawid Drozd aka Gelldur on 6/15/16.
 //
 
+#include <component/view/TextProtocol.h>
+
+#include <bridge/NativeObject.h>
+#include <bridge/Context.h>
 #include <log.h>
-#include "component/view/TextProtocol.h"
+#include <ColorUtils.h>
+
+#import <UIKit/UIKit.h>
 
 void TextProtocol::setText(const char* text)
 {
-	WLOG("Function %s not implemented: %s:%d", __func__, __FILE__, __LINE__);
+	auto context = getContext();
+
+	UILabel* label = context->getNative();
+	label.text = [NSString stringWithUTF8String:text];
 }
 
 void TextProtocol::setText(const std::string& text)
 {
-	WLOG("Function %s not implemented: %s:%d", __func__, __FILE__, __LINE__);
+	setText(text.c_str());
 }
 
 void TextProtocol::setTextColor(int color)
 {
-	WLOG("Function %s not implemented: %s:%d", __func__, __FILE__, __LINE__);
+	auto context = getContext();
+
+	UILabel* label = context->getNative();
+	label.textColor = UIColorFromRGB(color);
 }
 
 
