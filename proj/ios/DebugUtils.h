14976754a03c68342a94aea3ecc14a54d9112b20
Dawid Drozd <drozddawid.uam@gmail.com>
2016-06-27 16:50:25 +0200

    Add Debug utils

---
+++ proj/ios/DebugUtils.h
@@ -0,0 +1,12 @@
+//
+// Created by Dawid Drozd aka Gelldur on 6/27/16.
+//
+
+#pragma once
+#import <UIKit/UIKit.h>
+
+@interface DebugUtils : NSObject
+
++ (UIColor *)getRandomColor;
+
+@end
---
+++ proj/ios/DebugUtils.mm
@@ -0,0 +1,28 @@
+#import "DebugUtils.h"
+
+@implementation DebugUtils
+
++ (UIColor *) getRandomColor
+{
+	switch (rand() % 8)
+	{
+		case 0:
+			return [UIColor blueColor];
+		case 1:
+			return [UIColor greenColor];
+		case 2:
+			return [UIColor cyanColor];
+		case 3:
+			return [UIColor grayColor];
+		case 4:
+			return [UIColor redColor];
+		case 5:
+			return [UIColor yellowColor];
+		case 6:
+			return [UIColor orangeColor];
+		case 7:
+			return [UIColor magentaColor];
+	}
+}
+
+@end
