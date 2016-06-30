c5e479f25377436381feee8d548d624b561f2bb2
Dawid Drozd <drozddawid.uam@gmail.com>
2016-06-30 16:46:16 +0200

    Fix for linux log file

--- proj/linux/log.h
+++ proj/linux/log.h
@@ -6,8 +6,8 @@
 
 #include <cstdio>
 
-#define  DLOG(...)  printf("DLOG:\t");printf(__VA_ARGS__);printf("\n");fflush(stdout);
-#define  ILOG(...)  printf("ILOG:\t");printf(__VA_ARGS__);printf("\n");fflush(stdout);
-#define  WLOG(...)  printf("WLOG:\t");printf(__VA_ARGS__);printf("\n");fflush(stdout);
-#define  ELOG(...)  printf("ELOG:\t");printf(__VA_ARGS__);printf("\n");fflush(stdout);
-#define  FLOG(...)  printf("FLOG:\t");printf(__VA_ARGS__);printf("\n");fflush(stdout);
+#define  DLOG(...)  printf("DLOG:\t");printf(__VA_ARGS__);printf("\n");fflush(stdout)
+#define  ILOG(...)  printf("ILOG:\t");printf(__VA_ARGS__);printf("\n");fflush(stdout)
+#define  WLOG(...)  printf("WLOG:\t");printf(__VA_ARGS__);printf("\n");fflush(stdout)
+#define  ELOG(...)  printf("ELOG:\t");printf(__VA_ARGS__);printf("\n");fflush(stdout)
+#define  FLOG(...)  printf("FLOG:\t");printf(__VA_ARGS__);printf("\n");fflush(stdout)
