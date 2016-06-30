2c98429bdf1fead5f07a5f1d1f555564d5ec03b2
Dawid Drozd <drozddawid.uam@gmail.com>
2016-06-30 16:48:33 +0200

    Add iOS component impl

---
+++ proj/ios/component/view/List.mm
@@ -0,0 +1,29 @@
+//
+// Created by Dawid Drozd aka Gelldur on 6/15/16.
+//
+
+#include <component/view/List.h>
+
+#include <platform/Context.h>
+#include <log.h>
+
+#import <UIKit/UIKit.h>
+
+namespace Dexode
+{
+namespace Component
+{
+
+void List::reloadData()
+{
+	auto context = getContext();
+
+	UITableView* list = context->getNative();
+
+	// @formatter:off
+	[list reloadData];
+	// @formatter:on
+}
+
+}
+}
