0641b85d235f8f4cab384b92327d8b334a2b6dc1
Dawid Drozd <drozddawid.uam@gmail.com>
2016-07-05 10:40:20 +0200

    Improve iOSBuildHelpers.cmake

--- cmake/iOSBuildHelpers.cmake
+++ cmake/iOSBuildHelpers.cmake
@@ -1,15 +1,18 @@
 # Set the Base SDK (only change the SDKVER value, if for instance, you are building for iOS 5.0):
-SET(SDKVER "9.3")
+IF(NOT SDKVER)
+	MESSAGE(ERROR "Please define SDKVER variable for iOS")
+ENDIF()
+
 SET(DEVROOT "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer")
 SET(SDKROOT "${DEVROOT}/SDKs/iPhoneOS${SDKVER}.sdk")
 IF(EXISTS ${SDKROOT})
-	SET(CMAKE_OSX_SYSROOT "${SDKROOT}")
+	SET(CMAKE_OSX_SYSROOT "iphoneos${SDKVER}")
 ELSE()
 	MESSAGE("Warning, iOS Base SDK path not found: " ${SDKROOT})
 ENDIF()
 
 # Will resolve to "Standard (armv6 armv7)" on Xcode 4.0.2 and to "Standard (armv7)" on Xcode 4.2:
-SET(CMAKE_OSX_ARCHITECTURES "$(ARCHS_STANDARD_32_BIT)")
+SET(CMAKE_OSX_ARCHITECTURES "$(ARCHS_STANDARD)")
 
 # seamless toggle between device and simulator
 SET(CMAKE_XCODE_EFFECTIVE_PLATFORMS "-iphoneos;-iphonesimulator")
@@ -28,3 +31,14 @@ MACRO(ADD_FRAMEWORK fwname appname)
 		MESSAGE(STATUS "Framework ${fwname} found at ${FRAMEWORK_${fwname}}")
 	ENDIF()
 ENDMACRO(ADD_FRAMEWORK)
+
+# This little macro lets you set any XCode specific property.
+MACRO(SET_XCODE_PROPERTY TARGET XCODE_PROPERTY XCODE_VALUE)
+	SET_PROPERTY(TARGET ${TARGET} PROPERTY
+			XCODE_ATTRIBUTE_${XCODE_PROPERTY} ${XCODE_VALUE})
+ENDMACRO(SET_XCODE_PROPERTY)
+
+MACRO(SET_XCODE_PROPERTY_VARIANT TARGET XCODE_PROPERTY XCODE_VARIANT XCODE_VALUE)
+	SET_PROPERTY(TARGET ${TARGET} PROPERTY
+			XCODE_ATTRIBUTE_${XCODE_PROPERTY}[variant=${XCODE_VARIANT}] ${XCODE_VALUE})
+ENDMACRO(SET_XCODE_PROPERTY_VARIANT)
