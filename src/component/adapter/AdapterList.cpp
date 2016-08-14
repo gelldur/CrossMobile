9b775b05e6dda1e2c28aef14c0b2f886c020abc0
Dawid Drozd <drozddawid.uam@gmail.com>
2016-08-14 10:16:54 +0200

    Add AdapterList interface

---
+++ src/component/adapter/AdapterList.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Dawid Drozd aka Gelldur on 8/12/16.
+//
+
+#include "AdapterList.h"
+
+int AdapterList::getSize()
+{
+	return 0;
+}
+
+const std::string& AdapterList::getIdentifier(int index)
+{
+	static std::string overrideME;
+	return overrideME;
+}
+
+void AdapterList::setElement(int position, const Nodect& row)
+{
+}
---
+++ src/component/adapter/AdapterList.h
@@ -0,0 +1,21 @@
+//
+// Created by Dawid Drozd aka Gelldur on 8/12/16.
+//
+
+#pragma once
+
+#include <component/Nodect.h>
+
+class AdapterList
+{
+public:
+	virtual ~AdapterList() = default;
+
+	virtual int getSize();
+
+	virtual const std::string& getIdentifier(int index);
+
+	virtual void setElement(int position, const Nodect& row);
+};
+
+
