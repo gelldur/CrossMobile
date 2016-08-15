acca882bbfdee4054a0066d9cc4094f478c56d15
Dawid Drozd <drozddawid.uam@gmail.com>
2016-08-15 20:55:31 +0200

    Show logs only in release

--- proj/android/library/src/main/native/log.h
+++ proj/android/library/src/main/native/log.h
@@ -6,8 +6,20 @@
 
 #include <android/log.h>
 
+#ifdef DEBUG
+
 #define  DLOG(...)  __android_log_print(ANDROID_LOG_DEBUG,"JNI",__VA_ARGS__)
 #define  ILOG(...)  __android_log_print(ANDROID_LOG_INFO,"JNI",__VA_ARGS__)
 #define  WLOG(...)  __android_log_print(ANDROID_LOG_WARN,"JNI",__VA_ARGS__)
 #define  ELOG(...)  __android_log_print(ANDROID_LOG_ERROR,"JNI",__VA_ARGS__)
 #define  FLOG(...)  __android_log_print(ANDROID_LOG_FATAL,"JNI",__VA_ARGS__)
+
+#else
+
+#define  DLOG(...)
+#define  ILOG(...)
+#define  WLOG(...)  __android_log_print(ANDROID_LOG_WARN,"JNI",__VA_ARGS__)
+#define  ELOG(...)  __android_log_print(ANDROID_LOG_ERROR,"JNI",__VA_ARGS__)
+#define  FLOG(...)  __android_log_print(ANDROID_LOG_FATAL,"JNI",__VA_ARGS__)
+
+#endif
--- proj/ios/log.h
+++ proj/ios/log.h
@@ -6,8 +6,20 @@
 
 #include <cstdio>
 
+#ifdef DEBUG
+
 #define  DLOG(...)  fprintf(stderr,"DLOG:\t");fprintf(stderr,__VA_ARGS__);fprintf(stderr,"\n");fflush(stderr)
 #define  ILOG(...)  fprintf(stderr,"ILOG:\t");fprintf(stderr,__VA_ARGS__);fprintf(stderr,"\n");fflush(stderr)
 #define  WLOG(...)  fprintf(stderr,"WLOG:\t");fprintf(stderr,__VA_ARGS__);fprintf(stderr,"\n");fflush(stderr)
 #define  ELOG(...)  fprintf(stderr,"ELOG:\t");fprintf(stderr,__VA_ARGS__);fprintf(stderr,"\n");fflush(stderr)
 #define  FLOG(...)  fprintf(stderr,"FLOG:\t");fprintf(stderr,__VA_ARGS__);fprintf(stderr,"\n");fflush(stderr)
+
+#else
+
+#define  DLOG(...)
+#define  ILOG(...)
+#define  WLOG(...)  fprintf(stderr,"WLOG:\t");fprintf(stderr,__VA_ARGS__);fprintf(stderr,"\n");fflush(stderr)
+#define  ELOG(...)  fprintf(stderr,"ELOG:\t");fprintf(stderr,__VA_ARGS__);fprintf(stderr,"\n");fflush(stderr)
+#define  FLOG(...)  fprintf(stderr,"FLOG:\t");fprintf(stderr,__VA_ARGS__);fprintf(stderr,"\n");fflush(stderr)
+
+#endif
